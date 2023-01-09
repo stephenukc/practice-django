5ecdd096f051a4d6c4b714fb41e26fc5536ab70b
Stephen Kalu <stephenukc@gmail.com>
2023-01-09 15:49:31 +0100

    Add pub_date common name

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,13 @@
 import datetime
 
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
-from django.contrib import admin
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField()
+    pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.question_text
@@ -16,10 +17,10 @@ class Question(models.Model):
         ordering='pub_date',
         description='Published recently?',
     )
-    
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
